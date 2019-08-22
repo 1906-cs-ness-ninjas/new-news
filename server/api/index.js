18682d187421147254860a032a604a544bf354a7
Shan Hu <nahsuh@hotmail.com>
2019-08-22 18:02:45 -0400

    chrome extension favorite button working

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,8 @@ router.use('/topics', require('./routes/topics'))
 
 router.use('/bbcArticles', require('./routes/bbcArticles'))
 
+router.use('/favoriteSite', require('./routes/favoriteSite'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
