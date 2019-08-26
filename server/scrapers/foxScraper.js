3a583a68542d91ee344f76bc23fad358d54c1907
Shan Hu <38674188+uwsret@users.noreply.github.com>
2019-08-26 14:50:32 -0400

    Update foxScraper.js

--- server/scrapers/foxScraper.js
+++ server/scrapers/foxScraper.js
@@ -25,12 +25,13 @@ async function scrapeFoxHeadlines(page) {
         } else {
           imageUrl = 'http:' + img.src
         }
-      } else if (/media/g.test(pic)) {
+      } else {
+        if (/media/g.test(pic)) {
           imageUrl = pic
         } else {
           imageUrl = 'https:' + pic
         }
-
+      }
       let category = url.replace(/.*\/(.*?)\/.*/g, '$1')
 
       let article = {title: 'hello', imageUrl, url, category}
